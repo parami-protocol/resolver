cc88f186dfc46fceb0fb8f219183e85b8aab0c02
fy <qc90@foxmail.com>
2020-02-17 18:04:37 +0800

    update shell

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
+HOST=http://140.143.223.100:8091/api/reload
 git pull
 npm run build
 PORT=8091 npm run serve
+info=`curl $HOST -X POST -d '{"restart": true}' --header "Content-Type: application/json"`
+echo $info
