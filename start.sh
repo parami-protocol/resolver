5df219e58edab9a87017d91841b4d7de86d6011b
fy <qc90@foxmail.com>
2020-02-17 18:14:31 +0800

    update shell

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 HOST=http://140.143.223.100:8091/api/reload
+LOG_NAME="./sign.log"
 git pull
 npm run build
-PORT=8091 npm run serve
+#PORT=8091 npm run serve
+PORT=8091 npm run serve >> $LOG_NAME 2>&1 &  #将调试信息写入文件，并以后台的方式运行
 info=`curl $HOST -X POST -d '{"restart": true}' --header "Content-Type: application/json"`
 echo $info
