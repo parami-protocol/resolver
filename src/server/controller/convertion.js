cd542f2c43bc06b99b545cd8d0161a498991f392
qc90 <qc90@foxmail.com>
2020-07-04 16:52:18 +0800

    update

--- src/server/controller/convertion.js
+++ src/server/controller/convertion.js
@@ -1,28 +1,35 @@
 import { blake2AsHex } from '@polkadot/util-crypto'
+const { hexToU8a, stringToU8a, u8aConcat } = require('@polkadot/util')
 import { hexToDid } from 'libs/util'
 
 export default async (req, res, api) => {
   try {
     const { type, identifier } = req.query
-    let didHash
+    let didHash, rawHash
     if (type === 'unionid') {
-      const wxHash = blake2AsHex(identifier, 256)
-      const hash = blake2AsHex(`${wxHash}1`, 256)
-      didHash = await api.query.did.socialAccount(hash)
+      rawHash = blake2AsHex(identifier, 256)
+      const accountHash = blake2AsHex(`${rawHash}1`, 256)
+      didHash = await api.query.did.socialAccount(accountHash)
     } else if (type === 'index') {
-      const hash = blake2AsHex(identifier, 256)
-      didHash = await api.query.did.userKeys(hash)
+      rawHash = blake2AsHex(identifier, 256)
+      didHash = await api.query.did.userKeys(rawHash)
     } else { // did hash
       didHash = identifier
     }
-
-    const metadata = await api.query.did.metadata(didHash)
+    let metadata = await api.query.did.metadata(didHash)
+    if (metadata.isEmpty) {
+      const np = u8aConcat(hexToU8a(rawHash), stringToU8a('1'))
+      const finalParam = blake2AsHex(np, 256)
+      console.log(rawHash, finalParam, 'query again-----')
+      metadata = await api.query.did.metadata(finalParam)
+    }
     const result = hexToDid(metadata.did.toString())
 
     res.json({
       result
     })
   } catch (error) {
+    console.log(error)
     res.json({
       code: 0,
       msg: 'convertion failed',
